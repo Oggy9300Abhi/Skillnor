849c286787fa66ed55e52df2d74a01441ad9698d
Oggy9300Abhi <161196574+Oggy9300Abhi@users.noreply.github.com>
2026-06-16 09:20:37 +0530

    Add SplashActivity.kt

---
+++ app/src/main/java/com/skillnor/app/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.skillnor.app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    private val auth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        lifecycleScope.launch {
+            delay(2000)
+            val currentUser = auth.currentUser
+            val intent = if (currentUser != null) {
+                Intent(this@SplashActivity, MainActivity::class.java)
+            } else {
+                Intent(this@SplashActivity, LoginActivity::class.java)
+            }
+            startActivity(intent)
+            finish()
+        }
+    }
+}
